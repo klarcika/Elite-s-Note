8473fe500a96de2849183d4c8581fa5e1190a8db
KLARA KIRBIS <klara.kirbis@student.um.si>
2022-12-17 21:56:52 +0100

    Dodajanje uporbnika v skupino #ESN-79 DONE Time 15min

--- classes/src/main/java/com/example/elitesnote/kontrolerji/SkupinaController.java
+++ classes/src/main/java/com/example/elitesnote/kontrolerji/SkupinaController.java
@@ -3,7 +3,7 @@ package com.example.elitesnote.kontrolerji;
 import com.example.elitesnote.dao.SkupinaRepository;
 import com.example.elitesnote.razredi.Skupina;
 import com.example.elitesnote.razredi.Uporabnik;
-import com.example.elitesnote.razredi.Vsebina;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
